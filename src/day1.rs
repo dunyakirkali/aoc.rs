82ac4a579c78bbe0a5b14795db64a47a9d153aa3
dunyakirkali <dunyakirkali@gmail.com>
2022-09-25 22:54:28 +0200

    Extract Day 1

---
+++ src/day1.rs
@@ -0,0 +1,30 @@
+pub mod day1 {
+  pub fn part1(input: &String) {
+    println!(
+        "{:?}",
+        input
+            .lines()
+            .map(|n| n.parse::<u16>().unwrap())
+            .collect::<Vec<u16>>()
+            .array_windows::<2>()
+            .filter(|[a, b]| a < b)
+            .count()
+     );
+  }
+
+  pub fn part2(input: &String) {
+    println!(
+    "{:?}",
+    input
+      .lines()
+      .map(|n| n.parse::<u16>().unwrap())
+      .collect::<Vec<u16>>()
+      .array_windows::<3>()
+      .map(|list| list.iter().sum::<u16>())
+      .collect::<Vec<u16>>()
+      .array_windows::<2>()
+      .filter(|[a, b]| a < b)
+      .count()
+    );
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,19 @@
 #![feature(array_windows)]
 use std::fs;
 
-fn day1a() {
-    let file_path = String::from("priv/day1.txt");
+mod day1;
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+pub use crate::day1::day1::part1;
+pub use crate::day1::day1::part2;
+
+fn day1() {
+    let file_path = String::from("priv/day1/input.txt");
+    let input = fs::read_to_string(file_path).expect("Should have been able to read the file");
     
-     println!(
-        "{:?}",
-        contents
-            .lines()
-            .map(|n| n.parse::<u16>().unwrap())
-            .collect::<Vec<u16>>()
-            .array_windows::<2>()
-            .filter(|[a, b]| a < b)
-            .count()
-     );
-}
-
-fn day1b() {
-    let file_path = String::from("priv/day1.txt");
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    
-     println!(
-        "{:?}",
-        contents
-            .lines()
-            .map(|n| n.parse::<u16>().unwrap())
-            .collect::<Vec<u16>>()
-            .array_windows::<3>()
-            .map(|list| list.iter().sum::<u16>())
-            .collect::<Vec<u16>>()
-            .array_windows::<2>()
-            .filter(|[a, b]| a < b)
-            .count()
-     );
+    part1(&input);
+    part2(&input);
 }
 
 fn main() {
-    day1a();
-    day1b();
+    day1();
 }
